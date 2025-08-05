cb856a92597dcae8ef02652d7ea10a941d94a575
wsutil <wsu.uhctil@gmail.com>
2025-08-05 15:21:11 -0400

    testing.py

--- testing.py
+++ testing.py
@@ -81,7 +81,7 @@ def process_file(matpath, label_path, model, roi_extractor, clustering_layer, de
         auroc = roc_auc_score(labels, probs, multi_class='ovr')
         auprc = average_precision_score(labels, probs, average='macro')
     except ValueError as e:
-        print(f"AUROC / AUPRC 计算错误: {e}")
+        print(f"AUROC / AUPRC Error: {e}")
         auroc, auprc = None, None
 
     return precision, recall, f1, auroc, auprc
